be661f666fb09970c331e0f5c8b8a2551e6e392a
zack796 <zack796@gmail.com>
2018-10-18 22:01:21 -0500

    frontend changes
    eggdra pics removed
    assets folder
    index html

--- index.php
+++ index.php
@@ -1,38 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<link href="styles/main.css" rel="stylesheet"/>
-	<script src="js/eggScript.js"></script>
-	<title>Form</title>
-</head>
-<body id="page">
-	<img id="papi" width="80" height= "60">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mufalo & Xaz">
+    <link rel="icon" href="../../../../favicon.ico">
+    <!-- Bootstrap core CSS -->
+    <link href="../../css/editor.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="narrow-jumbotron.css" rel="stylesheet">
+  </head>
+	<!-- Navbar Style -->
+	<style>
+	body {margin:0;}
+	ul {
+	    list-style-type: none;
+	    margin: 0;
+	    padding: 0;
+	    overflow: hidden;
+	    background-color: #222;
+	    position: fixed;
+	    top: 0;
+	    width: 100%;
+	}
+	li {
+	    float: left;
+	}
+	li a {
+	    display: block;
+	    color: white;
+	    text-align: center;
+	    padding: 14px 16px;
+	    text-decoration: none;
+	}
+	li a:hover:not(.active) {
+	    background-color: #444;
+	}
+	.active {
+	    background-color: #00853D;
+	}
+	</style>
 
-	<p id="demo"></p>
+	<!-- Navbar List -->
+<ul>
+	<li><a class="active" href="#home">Home</a></li>
+	<li><a href="#contact">Discord</a></li>
+</ul>
 
-	<form action="process.php" method="post">
-		<h3>
-			<?php
-
-			if($_GET["error"] == "Nolambo"){
-
-				echo "Come back in 30 mins from last claim";
-			}
-
-			 ?>
-			 	
-			 </h3>
-		<div class="title">
-			
-			<p>EGGDRA FAUCET</p>
-		</div>
-    <div class="faucet">
-    	<input type="text" name="address" id="input">
-    	<br>
-    	<input type="submit" value="Claim" id="button" onClick="displayConfirmationPapi">
-		</div>
-
-	</form>
-</body>
+  <body style="font-family: 'Arial'; background-color: #212721;">
+		<!-- TurtleCoin Image -->
+    <div class="container">
+			<img src="assets/logo.png" style="display: block; margin-left: auto; margin-right: auto; padding: 80px;" height='250px' width='250px'>
+			<p style="font-family: 'Arial'; color: white; text-align: center; font-size: 36px;">TurtleCoin Faucet</p>
+			<!-- TurtleCoin Address input -->
+			<div class="container" style="text-align: center;">
+				<form style="">
+				<div class="form-group">
+					<input class="form-control" type="text" placeholder="TurtleCoin Address" style="max-width: 1000px; width: 75%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box; border: 1px solid #555;">
+				</div>
+			</form>
+			<!-- Hunter Code -->
+			<form style="">
+				<div class="form-group" style="">
+					<label></label>
+					<input class="form-control" type="text" placeholder="Hunter Code (Optional)" style="max-width: 1000px; width: 75%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box; border: 1px solid #555;">
+				</div>
+				<br>
+			</form>
+				<!-- Claim Button -->
+				<div class="container" style="">
+					<button type="button" class="btn btn-primary" style="cursor: pointer; background-color: #00853D; border: none; font-size: 24px; padding: 16px 24px; color: white;">Claim</button>
+					<div class="m-5"></div>
+				</div>
+			</div>
+			<!-- Footer -->
+      <footer class="footer" style="font-family: 'Arial'; position: fixed; left: 0; bottom: 0; color: white; text-align: center; width: 100%;">
+				<p>Made with love by <a href="chat.turtlecoin.lol" style="text-decoration: none; color: 00853D;">Mufalo</a> and <a href="xaznode.com" style="text-decoration: none; color: 00853D;">Xaz</a></p>
+      </footer>
+    </div> <!-- /container -->
+	</body>
 </html>
